024c89477ef25a16ab564c55c9aa79606325377c
Ben McNulty <benmcnulty@live.com>
2016-02-03 11:22:20 -0500

    Parsing JSON
    temp.html implements bend.js to pass values from menu.json to the
    browser.

--- js/bend.js
+++ js/bend.js
@@ -1,16 +1,30 @@
 (function() {
     "use strict";
     function loaded() {
-        var bender = document.getElementById("bender").innerHTML;
-        var girder = {
-            name: "Menu",
-            type: "JSON",
-            class: "girder"
-        };
-        
-        var output = Mustache.render(bender, girder);
-            document.getElementById("bender").innerHTML = output;
+
+        function loadJSON(callback) {
+            var xobj = new XMLHttpRequest();
+            xobj.overrideMimeType("application/json");
+            xobj.open('GET', 'js/menu.json', true);
+            xobj.onreadystatechange = function () {
+                if (xobj.readyState == 4 && xobj.status == "200") {
+                    callback(xobj.responseText);
+                    }
+            };
+            xobj.send(null);
         }
+
+        function bend() {
+            loadJSON(function(response) {
+                var bender = document.getElementById("bender").innerHTML;
+                var girder = JSON.parse(response);
+                var output = Mustache.render(bender, girder);
+                document.getElementById("bender").innerHTML = output;
+            });
+        }
+        
+        bend();
+    }
 
     window.addEventListener("load", loaded);
 })();
