887114e1cf64c50ffc5478dbb83a36811dc3124d
Ben McNulty <benmcnulty@live.com>
2016-02-03 16:51:43 -0500

    Finished JS template
    Applied template with mustache to render JSON data.

--- js/menuFetch.js
+++ js/menuFetch.js
@@ -5,7 +5,7 @@
         function loadJSON(callback) {
             var xobj = new XMLHttpRequest();
             xobj.overrideMimeType("application/json");
-            xobj.open('GET', 'js/menu.json', false);
+            xobj.open('GET', 'js/menu.json', true);
             xobj.onreadystatechange = function () {
                 if (xobj.readyState == 4 && xobj.status == "200") {
                     callback(xobj.responseText);
